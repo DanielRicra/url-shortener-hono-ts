7c7bbb0be1aeb0583732759b3c8e4933ddc3c577
DanielRicra <daniel.int@outlook.com>
2024-03-25 14:56:56 -0500

    Update to add db parameter to the functions

--- backend/src/infrastructure/repositories/short-url.repository.impl.ts
+++ backend/src/infrastructure/repositories/short-url.repository.impl.ts
@@ -1,3 +1,4 @@
+import type { Database } from "../../data/turso"
 import type { ShortenerUrlDatasource } from "../../domain/datasources"
 import type { ShortenerUrlRepository } from "../../domain/repositories"
 
@@ -6,10 +7,10 @@ export class ShortenerUrlRepositoryImpl implements ShortenerUrlRepository {
 		private readonly shortenerUrlDatasource: ShortenerUrlDatasource,
 	) {}
 
-	generatesShortUrl(longUrl: string): Promise<string> {
-		return this.shortenerUrlDatasource.generatesShortUrl(longUrl)
+	generatesShortUrl(longUrl: string, db: Database): Promise<string> {
+		return this.shortenerUrlDatasource.generatesShortUrl(longUrl, db)
 	}
-	getLongUrl(shortUrl: string): Promise<string> {
-		return this.shortenerUrlDatasource.getLongUrl(shortUrl)
+	getLongUrl(shortUrl: string, db: Database): Promise<string> {
+		return this.shortenerUrlDatasource.getLongUrl(shortUrl, db)
 	}
 }
