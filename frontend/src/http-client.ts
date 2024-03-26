4f301752f633144e417e2594c2774d92ed241db4
DanielRicra <daniel.int@outlook.com>
2024-03-25 21:05:36 -0500

    Update to use deploy api urls

--- frontend/src/http-client.ts
+++ frontend/src/http-client.ts
@@ -1,6 +1,6 @@
 class UrlShortenerService {
 	async shortLongUrl(longUrl: string): Promise<{ shortenedUrl: string }> {
-		const response = await fetch("http://localhost:8787/shorten-url", {
+		const response = await fetch("https://shorturl.danielquispe-142.workers.dev/shorten-url", {
 			method: "POST",
 			headers: {
 				"Content-Type": "application/json",
